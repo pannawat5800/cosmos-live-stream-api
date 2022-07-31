4c817db2a93812c3b20ca6b15455bae12eab6b9f
Pannawat Sonsom <pannawat5800@gmail.com>
2022-08-01 00:33:44 +0700

    change firebase

--- src/core/firebase.core.js
+++ src/core/firebase.core.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount =  require('../assets/firebase-credential.json')
+const serviceAccount = require('../assets/cosmos-9370a-firebase-adminsdk-dtnox-8f8bd0ab1e.json')
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 })
